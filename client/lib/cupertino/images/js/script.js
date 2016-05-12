d61fb7e7fe25302c5dd949ff5c599add107ec0cc
jiyoni1225 <jiyoni1225@gmail.com>
2016-05-12 16:53:08 +0000

    origin

--- client/lib/cupertino/images/js/script.js
+++ client/lib/cupertino/images/js/script.js
@@ -92,3 +92,20 @@ function kCalendar(id, date) {
 	
 	kCalendar.innerHTML = calendar;
 }
+
+/* Javascript */
+$(function () {
+	var date = new Date();
+	var d = date.getDate();
+	var m = date.getMonth();
+	var y = date.getFullYear();
+	
+	$('#calendar').fullCalendar({
+		header: {
+			left: 'prev,next today',
+			center: 'title',
+			right: 'month,agendaWeek,agendaDay'
+		},
+		editable: true
+	});
+});
